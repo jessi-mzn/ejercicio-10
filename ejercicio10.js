3db948b619576621b8f78bd81a34ced424d52cd8
jesica <113074000+jessi-mzn@users.noreply.github.com>
2022-09-17 21:25:51 -0300

    Add files via upload

---
+++ ejercicio10.js
@@ -0,0 +1,16 @@
+function promptdemo() {
+    
+    var number = prompt ('ingresa un numero');
+
+    if (number > 0) {
+     alert ('El numero ingresado es positivo');
+    }
+    else if (number < 0) {
+     alert('El numero ingresado es negativo');
+    }
+    
+    else { 
+      alert ('No ingreso un numero');
+    }
+    
+   }
